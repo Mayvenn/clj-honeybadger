22a9a95b21889821ea1a8ba89a965612af35fdc1
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-24 16:26:57 -0700

    Renaming private repository to releases

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
                  [cheshire "5.4.0"]
                  [clj-http "1.0.1"]
                  [clj-stacktrace "0.2.8"]]
-  :repositories [["private" {:url "s3p://mayvenn-dependencies/releases/"
-                             
-                             
-                             
+  
+                              
+                              
+                              
   :plugins [[s3-wagon-private "1.1.2"]]
   :profiles
   {:dev {:source-paths ["dev"]
