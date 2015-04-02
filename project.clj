ad82a34267d558fc501639befd3b56f9fa637427
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-02 14:14:52 -0700

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-honeybadger "0.3.0"
+(defproject clj-honeybadger "0.3.1-SNAPSHOT"
   :description "Sends exceptions to Honeybadger"
   :url "https://github.com/Mayvenn/clj-honeybadger"
   :license {:name "Eclipse Public License"
