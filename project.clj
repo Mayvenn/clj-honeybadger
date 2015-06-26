1a6f22163cd866b383a934e7682488efd2eba6fe
Corey Reece <corey@mayvenn.com>
2015-06-26 11:21:01 -0700

    Revert "Allow ring/request logging"
    This reverts commit b27e40678ab83f781829f23d1283b0d5d1d6c206.
    Possibly revisit in the future.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.4.0"]
-                 [ring/ring-core "1.3.2"]
                  [clj-http "1.0.1"]
                  [clj-stacktrace "0.2.8"]]
   
