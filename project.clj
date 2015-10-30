51690d04ee77bc6c00936683ddfe091fb8ce3f73
Jeff Hui <jeff@mayvenn.com>
2015-10-30 15:03:32 -0700

    Version 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-honeybadger "0.4.1-SNAPSHOT"
+(defproject clj-honeybadger "0.4.1"
   :description "Sends exceptions to Honeybadger"
   :url "https://github.com/Mayvenn/clj-honeybadger"
   :license {:name "Eclipse Public License"
